fab54db1c8d1bcbe141b6541bf7a134289d151fb
db2k <33785843+gazecube@users.noreply.github.com>
2026-09-14 09:36:47 -0700

    Add native Motif frontend entry point

---
+++ src/main.c
@@ -0,0 +1,9 @@
+/* Generated public-tree wrapper for the DEV-22.3 monolithic source. */
+#include "main_part01.inc"
+#include "main_part02.inc"
+#include "main_part03.inc"
+#include "main_part04.inc"
+#include "main_part05.inc"
+#include "main_part06.inc"
+#include "main_part07.inc"
+#include "main_part08.inc"

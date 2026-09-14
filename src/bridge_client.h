2506854dd066e5c63ed2ec0d7863e2814f242271
db2k <33785843+gazecube@users.noreply.github.com>
2026-09-14 11:35:31 -0700

    Align bridge client header with implementation

--- src/bridge_client.h
+++ src/bridge_client.h
@@ -5,14 +5,13 @@
 #include <sys/types.h>
 
 typedef struct {
-    char host[256];
-    unsigned short port;
+    char base_url[512];
     pid_t child_pid;
-    int managed;
 } BridgeClient;
 
 #define BRIDGE_START_OK 0
 #define BRIDGE_START_ERROR -1
+#define BRIDGE_START_FAILED BRIDGE_START_ERROR
 #define BRIDGE_START_INCOMPATIBLE -2
 #define BRIDGE_START_NODE_MISSING -3
 
@@ -24,8 +23,8 @@ int bridge_client_request(BridgeClient *client, const char *method,
                           char *response, size_t response_size);
 int bridge_client_request_bytes(BridgeClient *client, const char *method,
                                 const char *path, const char *body,
-                                unsigned char *response, size_t response_size,
-                                size_t *response_length);
+                                unsigned char **response,
+                                size_t *response_size);
 void bridge_client_stop(BridgeClient *client);
 
 #endif

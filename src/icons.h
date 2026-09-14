23ab3b51834d5110559ff838b34f78b3893f532b
db2k <33785843+gazecube@users.noreply.github.com>
2026-09-14 10:29:07 -0700

    Move icon definitions into implementation module

--- src/icons.h
+++ src/icons.h
@@ -1,201 +1,22 @@
-#ifndef MOTIF_APPLE_MUSIC_ICONS_H
-#define MOTIF_APPLE_MUSIC_ICONS_H
+#ifndef MARMELADE_ICONS_H
+#define MARMELADE_ICONS_H
 
-static char *play_icon_xpm[] = {
-"16 16 2 1", "  c None", "A c #000000",
-"                ", "                ", "  AA            ", "  AAAA          ",
-"  AAAAAA        ", "  AAAAAAAA      ", "  AAAAAAAAAA    ", "  AAAAAAAAAAAA  ",
-"  AAAAAAAAAAAA  ", "  AAAAAAAAAA    ", "  AAAAAAAA      ", "  AAAAAA        ",
-"  AAAA          ", "  AA            ", "                ", "                "
-};
-
-static char *previous_icon_xpm[] = {
-"16 16 2 1", "  c None", "A c #000000",
-"                ", "                ", "  AAA  AA       ", "  AAA  AAA      ",
-"  AAA  AAAA     ", "  AAA  AAAAA    ", "  AAA  AAAAAA   ", "  AAA  AAAAAAA  ",
-"  AAA  AAAAAAA  ", "  AAA  AAAAAA   ", "  AAA  AAAAA    ", "  AAA  AAAA     ",
-"  AAA  AAA      ", "  AAA  AA       ", "                ", "                "
-};
-
-static char *next_icon_xpm[] = {
-"16 16 2 1", "  c None", "A c #000000",
-"                ", "                ", "       AA  AAA  ", "      AAA  AAA  ",
-"     AAAA  AAA  ", "    AAAAA  AAA  ", "   AAAAAA  AAA  ", "  AAAAAAA  AAA  ",
-"  AAAAAAA  AAA  ", "   AAAAAA  AAA  ", "    AAAAA  AAA  ", "     AAAA  AAA  ",
-"      AAA  AAA  ", "       AA  AAA  ", "                ", "                "
-};
-
-static char *pause_icon_xpm[] = {
-"16 16 2 1", "  c None", "A c #000000",
-"                ", "                ", "   AAA    AAA   ", "   AAA    AAA   ",
-"   AAA    AAA   ", "   AAA    AAA   ", "   AAA    AAA   ", "   AAA    AAA   ",
-"   AAA    AAA   ", "   AAA    AAA   ", "   AAA    AAA   ", "   AAA    AAA   ",
-"   AAA    AAA   ", "   AAA    AAA   ", "                ", "                "
-};
-
-static char *volume_icon_xpm[] = {
-"16 16 2 1", "  c None", "A c #000000",
-"                ", "                ", "      A         ", "     AAA AAA    ",
-"    AAAA    A   ", "  AAAAAA AA  A  ", "  AAAAAA   A A  ", "  AAAAAA A A A  ",
-"  AAAAAA A A A  ", "  AAAAAA   A A  ", "  AAAAAA AA  A  ", "    AAAA    A   ",
-"     AAA AAA    ", "      A         ", "                ", "                "
-};
-
-static char *volume_medium_icon_xpm[] = {
-"16 16 2 1", "  c None", "A c #000000",
-"                ", "                ", "      A         ", "     AAA        ",
-"    AAAA        ", "  AAAAAA AA     ", "  AAAAAA   A    ", "  AAAAAA A A    ",
-"  AAAAAA A A    ", "  AAAAAA A A    ", "  AAAAAA   A    ", "    AAAA AA     ",
-"     AAA        ", "      A         ", "                ", "                "
-};
-
-static char *volume_low_icon_xpm[] = {
-"16 16 2 1", "  c None", "A c #000000",
-"                ", "                ", "      A         ", "     AAA        ",
-"    AAAA        ", "  AAAAAA        ", "  AAAAAA        ", "  AAAAAA A      ",
-"  AAAAAA A      ", "  AAAAAA A      ", "  AAAAAA        ", "    AAAA        ",
-"     AAA        ", "      A         ", "                ", "                "
-};
-
-static char *muted_icon_xpm[] = {
-"16 16 2 1", "  c None", "A c #000000",
-"                ", "                ", "      A         ", "     AAA        ",
-"    AAAA        ", "  AAAAAA        ", "  AAAAAA A   A  ", "  AAAAAA  A A   ",
-"  AAAAAA   A    ", "  AAAAAA  A A   ", "  AAAAAA A   A  ", "    AAAA        ",
-"     AAA        ", "      A         ", "                ", "                "
-};
-
-static char *shuffle_off_icon_xpm[] = {
-"20 20 2 1", "   c None", "A  c #000000",
-"                    ", "                    ", "                    ", "                A   ",
-"                 A  ", "  AAAA       AAA  A ", "      AA   AA    A  ", "        A       A   ",
-"         A          ", "         A          ", "          A     A   ", "      AA   AA    A  ",
-"  AAAA       AAA  A ", "                 A  ", "                A   ", "                    ",
-"                    ", "                    ", "                    ", "                    "
-};
-
-static char *shuffle_on_icon_xpm[] = {
-"20 20 2 1", "   c None", "A  c #1a5fb4",
-"                    ", "                    ", "                    ", "                A   ",
-"                 A  ", "  AAAA       AAA  A ", "      AA   AA    A  ", "        A       A   ",
-"         A          ", "         A          ", "          A     A   ", "      AA   AA    A  ",
-"  AAAA       AAA  A ", "                 A  ", "                A   ", "                    ",
-"                    ", "                    ", "                    ", "                    "
-};
-
-static char *repeat_off_icon_xpm[] = {
-"20 20 2 1", "   c None", "A  c #000000",
-"                    ", "                    ", "                    ", "                    ",
-"         A          ", "        A           ", "    AA A  AAAAAA    ", "   A    A       A   ",
-"  A      A       A  ", "  A              A  ", "  A              A  ", "  A       A      A  ",
-"   A       A    A   ", "    AAAAAA  A AA    ", "           A        ", "          A         ",
-"                    ", "                    ", "                    ", "                    "
-};
-
-static char *repeat_all_icon_xpm[] = {
-"20 20 2 1", "   c None", "A  c #1a5fb4",
-"                    ", "                    ", "                    ", "                    ",
-"         A          ", "        A           ", "    AA A  AAAAAA    ", "   A    A       A   ",
-"  A      A       A  ", "  A              A  ", "  A              A  ", "  A       A      A  ",
-"   A       A    A   ", "    AAAAAA  A AA    ", "           A        ", "          A         ",
-"                    ", "                    ", "                    ", "                    "
-};
-
-static char *repeat_one_icon_xpm[] = {
-"20 20 2 1", "   c None", "A  c #1a5fb4",
-"                    ", "                    ", "                    ", "                    ",
-"         A          ", "        A           ", "    AA A  AAAAAA    ", "   A    A       A   ",
-"  A      A       A  ", "  A              A  ", "  A              A  ", "  A              A  ",
-"   A            A   ", "    AAAAAAAAAAAA    ", "                    ", "                    ",
-"                    ", "                    ", "                    ", "                    "
-};
-
-static char *automix_off_icon_xpm[] = {
-"16 16 2 1",
-"   c None",
-"A  c #000000",
-"                ",
-"                ",
-"                ",
-"                ",
-"    AAA  AAA    ",
-"   A   AAAAAA   ",
-"  A     A  AAA  ",
-"  A         AA  ",
-"  A         AA  ",
-"  A     A  AAA  ",
-"   A   AAAAAA   ",
-"    AAA  AAA    ",
-"                ",
-"                ",
-"                ",
-"                "
-};
-
-static char *automix_on_icon_xpm[] = {
-"16 16 2 1",
-"   c None",
-"A  c #1a5fb4",
-"                ",
-"                ",
-"                ",
-"                ",
-"    AAA  AAA    ",
-"   A   AAAAAA   ",
-"  A     A  AAA  ",
-"  A         AA  ",
-"  A         AA  ",
-"  A     A  AAA  ",
-"   A   AAAAAA   ",
-"    AAA  AAA    ",
-"                ",
-"                ",
-"                ",
-"                "
-};
-
-static char *autoplay_off_icon_xpm[] = {
-"16 16 2 1",
-"   c None",
-"A  c #000000",
-"                ",
-"                ",
-"                ",
-"                ",
-"   AA      AA   ",
-"  A  A    A  A  ",
-" A    A  A    A ",
-" A      A     A ",
-" A     A      A ",
-" A    A  A    A ",
-"  A  A    A  A  ",
-"   AA      AA   ",
-"                ",
-"                ",
-"                ",
-"                "
-};
-
-static char *autoplay_on_icon_xpm[] = {
-"16 16 2 1",
-"   c None",
-"A  c #1a5fb4",
-"                ",
-"                ",
-"                ",
-"                ",
-"   AA      AA   ",
-"  A  A    A  A  ",
-" A    A  A    A ",
-" A      A     A ",
-" A     A      A ",
-" A    A  A    A ",
-"  A  A    A  A  ",
-"   AA      AA   ",
-"                ",
-"                ",
-"                ",
-"                "
-};
+extern char *play_icon_xpm[];
+extern char *previous_icon_xpm[];
+extern char *next_icon_xpm[];
+extern char *pause_icon_xpm[];
+extern char *volume_icon_xpm[];
+extern char *volume_medium_icon_xpm[];
+extern char *volume_low_icon_xpm[];
+extern char *muted_icon_xpm[];
+extern char *shuffle_off_icon_xpm[];
+extern char *shuffle_on_icon_xpm[];
+extern char *repeat_off_icon_xpm[];
+extern char *repeat_all_icon_xpm[];
+extern char *repeat_one_icon_xpm[];
+extern char *automix_off_icon_xpm[];
+extern char *automix_on_icon_xpm[];
+extern char *autoplay_off_icon_xpm[];
+extern char *autoplay_on_icon_xpm[];
 
 #endif
